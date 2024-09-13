267828a7adf7ee70bfe990c082dc9df455974b90
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-09-13 18:55:03 +0300

    Prevent Sql Injection, Sql parameters

--- 02_CRUD_Interface/Program.cs
+++ 02_CRUD_Interface/Program.cs
@@ -24,14 +24,20 @@ namespace _02_CRUD_Interface
         public void Create(Product product)
         {
             string cmdText = $@"INSERT INTO Products
-                              VALUES ('{product.Name}', 
-                                      '{product.Type}', 
-                                       {product.Quantity}, 
-                                       {product.CostPrice}, 
-                                      '{product.Producer}', 
-                                       {product.Price})";
+                              VALUES (@name, 
+                                      @Type, 
+                                      @Quantity, 
+                                      @CostPrice, 
+                                      @Producer, 
+                                      @Price)";
 
             SqlCommand command = new SqlCommand(cmdText, conn);
+            command.Parameters.AddWithValue("name", product.Name);
+            command.Parameters.AddWithValue("Type", product.Type);
+            command.Parameters.AddWithValue("Quantity", product.Quantity);
+            command.Parameters.AddWithValue("CostPrice", product.CostPrice);
+            command.Parameters.AddWithValue("Producer", product.Producer);
+            command.Parameters.AddWithValue("Price", product.Price);
             command.CommandTimeout = 5; // default - 30sec
 
 
@@ -46,67 +52,75 @@ namespace _02_CRUD_Interface
             SqlCommand command = new SqlCommand(cmdText, conn);
 
             SqlDataReader reader = command.ExecuteReader();
-
-            Console.OutputEncoding = Encoding.UTF8;
+            return this.GetProductByQuery(reader);
+        }
+        public List<Product> GetByName(string name)
+        {
+            //Stanga';drop database SportShop;--'
+            string cmdText = $@"select * from Products where Name = @name";
+            //name = "Ball"; 
+            SqlCommand command = new SqlCommand(cmdText, conn);
+            //command.Parameters.Add("name",System.Data.SqlDbType.NVarChar).Value = name; 
+            SqlParameter parameter = new SqlParameter()
+            {
+                ParameterName = "name",
+                SqlDbType = System.Data.SqlDbType.NVarChar,
+                Value = name
+            };
+            command.Parameters.Add(parameter);  
+            SqlDataReader reader = command.ExecuteReader();
+            return this.GetProductByQuery(reader);
+        }
+        private List<Product> GetProductByQuery(SqlDataReader reader)
+        { 
             List<Product> products = new List<Product>();
-           
+
             while (reader.Read())
             {
                 products.Add(new Product()
                 {
-                    Id =(int) reader[0],
-                    Name =(string) reader[1],
-                    Type =(string) reader[2],
-                    Quantity =(int) reader[3],
-                    CostPrice = (int) reader[4],
-                    Producer = (string) reader[5],
-                    Price =(int) reader[6]
+                    Id = (int)reader[0],
+                    Name = (string)reader[1],
+                    Type = (string)reader[2],
+                    Quantity = (int)reader[3],
+                    CostPrice = (int)reader[4],
+                    Producer = (string)reader[5],
+                    Price = (int)reader[6]
                 });
             }
-
             reader.Close();
             return products;
         }
         public Product GetById(int id)
         {
-            #region Execute Reader
+    
             string cmdText = $@"select * from Products where Id = {id}";
 
             SqlCommand command = new SqlCommand(cmdText, conn);
 
             SqlDataReader reader = command.ExecuteReader();
 
-            Product product = new Product();
-
-            while (reader.Read())
-            {
-
-                product.Id = (int)reader[0];
-                product.Name = (string)reader[1];
-                product.Type = (string)reader[2];
-                product.Quantity = (int)reader[3];
-                product.CostPrice = (int)reader[4];
-                product.Producer = (string)reader[5];
-                product.Price = (int)reader[6];
-
-            }
-            reader.Close();
-            return product;
-            #endregion
+            return this.GetProductByQuery(reader).FirstOrDefault()!;
 
         }
         public void Update(Product product)
         {
             string cmdText = $@"UPDATE Products
-                              SET Name ='{product.Name}', 
-                                  TypeProduct ='{product.Type}', 
-                                  Quantity ={product.Quantity}, 
-                                  CostPrice ={product.CostPrice}, 
-                                  Producer ='{product.Producer}', 
-                                  Price ={product.Price}
+                              SET Name =@name, 
+                                  TypeProduct =@Type, 
+                                  Quantity =@Quantity, 
+                                  CostPrice =@CostPrice, 
+                                  Producer =@Producer, 
+                                  Price =@Price
                                   where Id = {product.Id}";
 
             SqlCommand command = new SqlCommand(cmdText, conn);
+            command.Parameters.AddWithValue("name", product.Name);
+            command.Parameters.AddWithValue("Type", product.Type);
+            command.Parameters.AddWithValue("Quantity", product.Quantity);
+            command.Parameters.AddWithValue("CostPrice", product.CostPrice);
+            command.Parameters.AddWithValue("Producer", product.Producer);
+            command.Parameters.AddWithValue("Price", product.Price);
             command.CommandTimeout = 5; // default - 30sec
 
             command.ExecuteNonQuery();
@@ -140,17 +154,18 @@ namespace _02_CRUD_Interface
                 Price = 200
             };
             //sportShop.Create(pr);
-
-            var products = sportShop.GetALL();
+            Console.WriteLine("Enter name of product to search : ");
+            string name = Console.ReadLine()!;
+            var products = sportShop.GetByName(name);
             foreach (var product in products) {
                 Console.WriteLine(product);
             }
 
             sportShop.Delete(47);
 
-           var changeProduct =  sportShop.GetById(2);
-            changeProduct.Price = 500;
-            changeProduct.CostPrice = 300;
+           var changeProduct =  sportShop.GetById(3);
+            changeProduct.Price += 500;
+            changeProduct.CostPrice += 300;
             sportShop.Update(changeProduct);    
         }
     }
